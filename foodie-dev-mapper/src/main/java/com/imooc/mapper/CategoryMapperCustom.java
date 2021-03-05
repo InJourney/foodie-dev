8d81b3b9f2d7208eeaa94ad9a21b9a03cabdb4d5
ljt <2027154490@qq.com>
2021-03-05 17:43:44 +0800

    foodie-dev verson 1.0

--- foodie-dev-mapper/src/main/java/com/imooc/mapper/CategoryMapperCustom.java
+++ foodie-dev-mapper/src/main/java/com/imooc/mapper/CategoryMapperCustom.java
@@ -1,7 +1,8 @@
 package com.imooc.mapper;
 
-import com.imooc.my.mapper.MyMapper;
-import com.imooc.pojo.Category;
 
-public interface CategoryMapperCustom extends MyMapper<Category> {
+import java.util.List;
+
+public interface CategoryMapperCustom  {
+    public List getSubCatList(Integer rootCatId);
 }
